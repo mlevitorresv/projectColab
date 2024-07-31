b04898a1038c8b1de9e1b5aeda1d74d4e5d66066
mlevitorresv <levitorres66@gmail.com>
2024-07-31 15:12:54 +0200

    routes moved from web.php to api.php

--- API/app/Http/Controllers/AuthController.php
+++ API/app/Http/Controllers/AuthController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+
+
+
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validateData['name'],
+            'email' => $validateData['email'],
+            'password' => Hash::make($validateData['password']),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(
+            ['user' => $user, 'token' => $token],
+            201
+        );
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+    }
+
+    public function logout(){
+        Auth::guard('web')->logout();
+        return response()->josn(['message' => 'Succesfully logout']);
+    }
 }
