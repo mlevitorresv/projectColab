00c72434f06d2c6c4d6287baf651c668ce932bc1
mlevitorresv <levitorres66@gmail.com>
2024-07-31 15:27:13 +0200

    routes without /api

--- API/routes/api.php
+++ API/routes/api.php
@@ -11,41 +11,41 @@ use App\Http\Controllers\AuthController;
 
 // Public routes
 //Authentication Routes
-Route::post('/api/login', [AuthController::class, 'login']);
-Route::post('/api/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 // Private Routes
 Route::middleware(['jwt.auth', ApiMiddleware::class])->group(function () {
     //Authentication Routes
-    Route::post('/api/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
 
     // Users Routes
-    Route::get('/api/users', [UsersController::class, 'index']);
-    Route::post('/api/users', [UsersController::class, 'store']);
-    Route::get('/api/users/{id}', [UsersController::class, 'show']);
-    Route::put('/api/users/{id}', [UsersController::class, 'update']);
-    Route::delete('/api/users/{id}', [UsersController::class, 'destroy']);
+    Route::get('/users', [UsersController::class, 'index']);
+    Route::post('/users', [UsersController::class, 'store']);
+    Route::get('/users/{id}', [UsersController::class, 'show']);
+    Route::put('/users/{id}', [UsersController::class, 'update']);
+    Route::delete('/users/{id}', [UsersController::class, 'destroy']);
 
 
     // Projects Routes
-    Route::get('/api/projects', [ProjectsController::class, 'index']);
-    Route::post('/api/projects', [ProjectsController::class, 'store']);
-    Route::get('/api/projects/{id}', [ProjectsController::class, 'show']);
-    Route::put('/api/projects/{id}', [ProjectsController::class, 'update']);
-    Route::delete('/api/projects/{id}', [ProjectsController::class, 'destroy']);
+    Route::get('/projects', [ProjectsController::class, 'index']);
+    Route::post('/projects', [ProjectsController::class, 'store']);
+    Route::get('/projects/{id}', [ProjectsController::class, 'show']);
+    Route::put('/projects/{id}', [ProjectsController::class, 'update']);
+    Route::delete('/projects/{id}', [ProjectsController::class, 'destroy']);
 
     // Tasks Routes
-    Route::get('/api/tasks', [TasksController::class, 'index']);
-    Route::post('/api/tasks', [TasksController::class, 'store']);
-    Route::get('/api/tasks/{id}', [TasksController::class, 'show']);
-    Route::put('/api/tasks/{id}', [TasksController::class, 'update']);
-    Route::delete('/api/tasks/{id}', [TasksController::class, 'destroy']);
+    Route::get('/tasks', [TasksController::class, 'index']);
+    Route::post('/tasks', [TasksController::class, 'store']);
+    Route::get('/tasks/{id}', [TasksController::class, 'show']);
+    Route::put('/tasks/{id}', [TasksController::class, 'update']);
+    Route::delete('/tasks/{id}', [TasksController::class, 'destroy']);
 
     // Comunications Routes
-    Route::get('/api/communications', [CommunicationsController::class, 'index']);
-    Route::post('/api/communications', [CommunicationsController::class, 'store']);
-    Route::get('/api/communications/{id}', [CommunicationsController::class, 'show']);
-    Route::delete('/api/communications/{id}', [CommunicationsController::class, 'destroy']);
+    Route::get('/communications', [CommunicationsController::class, 'index']);
+    Route::post('/communications', [CommunicationsController::class, 'store']);
+    Route::get('/communications/{id}', [CommunicationsController::class, 'show']);
+    Route::delete('/communications/{id}', [CommunicationsController::class, 'destroy']);
 
 });
